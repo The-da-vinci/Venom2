678ea4ee212d6ffc34207f1e4d7dd843308454c4
the-da-vinci <kulta.kala@gmail.com>
2019-04-01 13:02:27 +0300

    fixing scanning and reformatted

--- venom2.py
+++ venom2.py
@@ -5,7 +5,7 @@ import random
 import sys
 import os
 import asyncio
-import datetime
+from datetime import datetime
 import requests
 import re
 
@@ -13,14 +13,38 @@ version = '.0.1'
 proxyenabled = False
 
 try:
-    d0rk = [line.strip() for line in open("lists/d0rks", "r", encoding="utf-8")]
-    header = [line.strip() for line in open("lists/header", "r", encoding="utf-8")]
-    xsses = [line.strip() for line in open("lists/xsses", "r", encoding="utf-8")]
-    lfis = [line.strip() for line in open("lists/pathto_huge.txt", "r", encoding="utf-8")]
-    tables = [line.strip() for line in open("lists/tables", "r", encoding="utf-8")]
-    columns = [line.strip() for line in open("lists/columns", "r", encoding="utf-8")]
-    search_Ignore = [line.strip() for line in open("lists/ignore", "r", encoding="utf-8")]
-    random.shuffle(d0rk)
+    full_path = os.path.realpath(__file__)
+    path, filename = os.path.split(full_path)
+    dorks = [
+        line.strip()
+        for line in open(path + "\\lists\\d0rks", "r", encoding="utf-8")
+    ]
+    header = [
+        line.strip()
+        for line in open(path + "\\lists\\header", "r", encoding="utf-8")
+    ]
+    xsses = [
+        line.strip()
+        for line in open(path + "\\lists\\xsses", "r", encoding="utf-8")
+    ]
+    lfis = [
+        line.strip()
+        for line in open(
+            path + "\\lists\\pathto_huge.txt", "r", encoding="utf-8")
+    ]
+    tables = [
+        line.strip()
+        for line in open(path + "\\lists\\tables", "r", encoding="utf-8")
+    ]
+    columns = [
+        line.strip()
+        for line in open(path + "\\lists\\columns", "r", encoding="utf-8")
+    ]
+    search_Ignore = [
+        line.strip()
+        for line in open(path + "\\lists\\ignore", "r", encoding="utf-8")
+    ]
+    random.shuffle(dorks)
     random.shuffle(header)
     random.shuffle(lfis)
 except Exception as err:
@@ -30,14 +54,12 @@ except Exception as err:
 
 class menus:
     def __init__(self):
-        if len(sys.argv) > 0:
-            self.tld = arg1
-            self.dorks = arg2
-            self.threads = arg3
-            self.load_dorks()
+        if len(sys.argv) > 2:
+            self.tld = sys.argv[1]
+            self.dorks = sys.argv[2]
+            self.threads = sys.argv[3]
             self.s = scanning()
         else:
-            self.load_dorks()
             self.s = scanning()
 
     def usage():
@@ -58,12 +80,13 @@ class menus:
         print("         Proxy Enabled " + " [", proxyenabled, "] ")
         print("---------------------------------------------------")
 
-    def pre_run(self):
-        if func == 'scan':
-            ending = arg1
-            dorks = arg2
-            threads = arg3
-            self.s.scan(arg1, arg2, arg3)
+
+#    def pre_run(self):
+#        if func == 'scan':
+#            ending = arg1
+#            dorks = arg2
+#            threads = arg3
+#            self.s.scan(arg1, arg2, arg3)
 
     def main_menu(self):
         self.logo()
@@ -93,99 +116,137 @@ def main():
         except KeyboardInterrupt or Exception as err:
             print(err)
             exit()
+            datetime.now()
 
 
 class scanning:
     def __init__(self):
-        self.arg1 = arg1
-        self.arg2 = arg2
-        self.arg3 = arg3
-        self.m = menus()
+        if len(sys.argv) > 1:
+            self.arg1 = arg1
+            self.arg2 = arg2
+            self.arg3 = arg3
         self.Final_list = []
-        self.tld
+        self.tld = ''
         self.dorks_in_memory = []
+        self.dork = ''
+        self.site = ''
         self.time_now = datetime.now()
-        self.search = "http://www.bing.com/search?q="
-        self.page_num = "&go=Submit&first="
-        self.query_last = "&count=50"
         self.crawled_sites = []
-        self.query = '{}+site:{}'.format(self.dork, self.site)
         self.progress = '{}/{}'.format(self.dork, self.dorks_in_memory)
-        self.vuln_list = ['error in your SQL syntax', 'mysql_fetch', 'num_rows', 'ORA-01756',
-                          'Error Executing Database Query', 'SQLServer JDBC Driver',
-                          'OLE DB Provider for SQL Server', 'Unclosed quotation mark',
-                          'ODBC Microsoft Access Driver', 'Microsoft JET Database',
-                          'Error Occurred While Processing Request', 'Microsoft JET Database',
-                          'Server Error', 'ODBC Drivers error', 'Invalid Querystring',
-                          'OLE DB Provider for ODBC', 'VBScript Runtime', 'ADODB.Field',
-                          'BOF or EOF', 'ADODB.Command', 'JET Database', 'mysql_fetch_array',
-                          'Syntax error', 'mysql_numrows()', 'GetArray()', 'FetchRow()',
-                          'Input string was not in a correct format']
+        self.vuln_list = [
+            'error in your SQL syntax', 'mysql_fetch', 'num_rows', 'ORA-01756',
+            'Error Executing Database Query', 'SQLServer JDBC Driver',
+            'OLE DB Provider for SQL Server', 'Unclosed quotation mark',
+            'ODBC Microsoft Access Driver', 'Microsoft JET Database',
+            'Error Occurred While Processing Request',
+            'Microsoft JET Database', 'Server Error', 'ODBC Drivers error',
+            'Invalid Querystring', 'OLE DB Provider for ODBC',
+            'VBScript Runtime', 'ADODB.Field', 'BOF or EOF', 'ADODB.Command',
+            'JET Database', 'mysql_fetch_array', 'Syntax error',
+            'mysql_numrows()', 'GetArray()', 'FetchRow()',
+            'Input string was not in a correct format'
+        ]
 
-        self.vuln_to = ['MySQL Classic', 'MiscError', 'MiscError2', 'Oracle', 'JDBC_CFM',
-                        'JDBC_CFM2', 'MSSQL_OLEdb', 'MSSQL_Uqm', 'MS-Access_ODBC',
-                        'MS-Access_JETdb', 'Processing Request', 'MS-Access JetDb',
-                        'Server Error', 'ODBC Drivers error', 'Invalid Querystring',
-                        'OLE DB Provider for ODBC', 'VBScript Runtime', 'ADODB.Field', 'BOF or EOF',
-                        'ADODB.Command', 'JET Database', 'mysql_fetch_array', 'Syntax error',
-                        'mysql_numrows()', 'GetArray()', 'FetchRow()', 'Input String Error']
+        self.vuln_to = [
+            'MySQL Classic', 'MiscError', 'MiscError2', 'Oracle', 'JDBC_CFM',
+            'JDBC_CFM2', 'MSSQL_OLEdb', 'MSSQL_Uqm', 'MS-Access_ODBC',
+            'MS-Access_JETdb', 'Processing Request', 'MS-Access JetDb',
+            'Server Error', 'ODBC Drivers error', 'Invalid Querystring',
+            'OLE DB Provider for ODBC', 'VBScript Runtime', 'ADODB.Field',
+            'BOF or EOF', 'ADODB.Command', 'JET Database', 'mysql_fetch_array',
+            'Syntax error', 'mysql_numrows()', 'GetArray()', 'FetchRow()',
+            'Input String Error'
+        ]
 
-    def scan(self, arg1, arg2, arg3):
-        if len(arg1) > 0:
-            pass
-        else:
-            self.tld = input('\nChoose your target domain for example ".com"\r\n :')
-            self.sitearray = [self.tld]
-            dorks = int(input("Choose the number of dorks (0 for all)\r\n: "))
-            try:
-                if int(dorks) == 0:
-                    i = 0
-                    while i < len(dorks):
-                        self.dorks_in_memory.append(dorks[i])
-                        i += 1
-                else:
-                    i = 0
-                    while i < int(dorks):
-                        self.dorks_in_memory.append(dorks[i])
-                        i += 1
-            except Exception as err:
-                print(err)
-                return
-            self.Threads = int(input('How many threads do you want to use?'))
-            self.Number_of_pages = int(input("Enter number of pages to go through\n :"))
-            self.loop = asyncio.get_event_loop()
-            self.usearch = self.loop.run_until_complete(self.crawl(self.Number_of_pages))
+    def scan(self):
+        try:
+            if len(sys.argv) > 1:
+                pass
+            else:
+                self.tld = input(
+                    '\nChoose your target domain or a search word ("*.com")\r\n:'
+                )
+                self.sitearray = [self.tld]
+                dork_count = int(
+                    input("Choose the number of dorks (0 for all)\r\n:"))
+                try:
+                    if dork_count == 0:
+                        i = 0
+                        while i < dork_count:
+                            self.dorks_in_memory.append(dorks[i])
+                            i += 1
+                    else:
+                        i = 0
+                        while i < dork_count:
+                            self.dorks_in_memory.append(dorks[i])
+                            i += 1
+                except Exception as err:
+                    print(err)
+                    return
+                self.Threads = int(
+                    input('How many threads do you want to use?\n:'))
+                self.Number_of_pages = int(
+                    input("Enter number of pages to go through\n:"))
+                self.loop = asyncio.get_event_loop()
+                self.usearch = self.loop.run_until_complete(self.crawl())
+        except KeyboardInterrupt:
+            exit()
 
     async def crawl(self):
         for site in self.sitearray:
             for dork in self.dorks_in_memory:
                 page = 0
-                while page < int(self.Number_of_pages):
-                    for i in self.Number_of_pages:
-                        loop = asyncio.get_event_loop()
-                        Results = self.crawled_sites.append("{}{}{}{}{}".format(self.search, self.query, self.page_num, (i * 50 + 1), self.query_last))
-                        page += 1
-                        self.crawled_sites.append(loop.run_in_executor(None, self.ignoringGet, self.crawled_sites))
-                    stringreg = re.compile('(?<=href=")(.*?)(?=")')
-                    for site in self.crawled_sites:
-                        for i in self.search_Ignore:
+                query = '{}+site:{}'.format(dork, site)
+                while page < self.Number_of_pages:
+                    loop = asyncio.get_event_loop()
+                    futures = []
+                    Results = (
+                        "http://www.bing.com/search?q=" + query +
+                        "&go=Submit&first=" + str(page * 50 + 1) + "&count=50")
+                    page += 1
+                    print(Results)
+                    if Results is not None:
+                        futures.append(
+                            loop.run_in_executor(None, self.checkdead,
+                                                 Results))
+                stringreg = re.compile('(?<=href=")(.*?)(?=")')
+                for future in futures:
+                    result = await future
+                    self.crawled_sites.extend(stringreg.findall(result))
+                    for url in self.crawled_sites:
+                        for i in search_Ignore:
                             basename = re.search(r"(?<=(://))[^/]*(?=/)", site)
-                            if ((basename is None) or re.search(i, site)):
+                            if ((basename is None) or re.search(i, url)):
                                 basename = re.search(r"(?<=://).*", i)
                             if basename is not None:
                                 basename = basename.group(0)
-                            if basename not in domains and basename is not None:
-                                domains.add(basename)
-                                urls.append(name)
+                            if basename not in self.Final_list and basename is not None:
+                                print(basename, url)
+                                self.Final_list.append(url)
+                m = menus()
+                m.logo()
+                percent = int(
+                    (1.0 * self.progress / int(len(self.dorks_in_memory))) *
+                    100)
+                print("| Target: <%s> \r\n"
+                      "| Collected urls: <%s> \r\n"
+                      "| D0rks: <%s/%s> Progressed so far \r\n"
+                      "| Percent Done: <%s> \r\n"
+                      "| Dork In Progress: <%s> \r\n"
+                      "| Elapsed Time: <%s> \r\n" % (self.tld,
+                                                     len(self.Final_list),
+                                                     self.progress,
+                                                     repr(percent), dork,
+                                                     datetime.now()))
 
-    def ignoringGet(self, url):
+    def checkdead(self, url):
         try:
-            responce = requests.get(url, timeout=2)
-            responce.raise_for_status()
+            response = requests.get(url, timeout=2)
+            response.raise_for_status()
         except Exception as verb:
             print(str(verb))
         finally:
-            return responce.text
+            return response.text
 
 
 # main program code here #
@@ -194,14 +255,14 @@ if __name__ == "__main__":
     running = True
     print(sys.argv)
     if len(sys.argv) >= 2:
-            try:
-                func = str(sys.argv[1])
-                arg1 = str(sys.argv[2])
-                arg2 = sys.argv[3]
-                arg3 = sys.argv[4]
-                m.pre_run()
-            except Exception as err:
-                print(err)
-                m.usage()
+        try:
+            func = str(sys.argv[1])
+            arg1 = str(sys.argv[2])
+            arg2 = sys.argv[3]
+            arg3 = sys.argv[4]
+            m.pre_run()
+        except Exception as err:
+            print(err)
+            m.usage()
     else:
         main()
