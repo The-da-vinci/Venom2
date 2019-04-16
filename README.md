71fab349eb839e010b87a9bcc017cd71694a903b
the-da-vinci <leonard.nyholm@atea.fi>
2019-04-16 12:49:42 +0300

    added legal disclaimer and lisence to venom2

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Venom2
 
-## Introduction
+Legal Disclaimer
+----
+Usage of Venom2 for finding targets is illegal. It is the end user's responsibility to obey all applicable local, state and
+federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program!
+
+Introduction
+----
 V3n0M is a free and open source scanner, that automates the process of finding possibly SQL injection vulnerable URLs. It harnesses the power of async, and proxy support to scour the interwebs.
 
-##Installation
+Installation
+----
 Preferably, you can download sqlmap by cloning the Git repository:
 
     git clone https://github.com/The-da-vinci/Venom2.git
@@ -11,11 +18,10 @@ Preferably, you can download sqlmap by cloning the Git repository:
     python3 -m pip install -r requirements.txt
     python3 venom2.py
 
-Venom2 works out of the box with [Python](http://www.python.org/download/) version **3.6.x** and **3.7.x** on any platform.
+Venom2 works almost out of the box with [Python](http://www.python.org/download/) version **3.6.x** and **3.7.x**.
 
-##Usage
+Usage
 ----
-
 To get a list of basic options and switches use:
 
     python3 venom2.py -h
@@ -30,6 +36,6 @@ To get a list of basic options and switches use:
     -t TARGET, --target TARGET
                         Targeted Top Level Domain
     -d DORKS, --dorks DORKS
-                        amount of dorks
+                        Amount of dorks
     -P PAGES, --pages PAGES
                         Number of pages to go through
--- venom2.py
+++ venom2.py
@@ -1,4 +1,35 @@
 #!/usr/bin/python3
+#
+# Venom2
+# Copyright (C) 2019  Da-vinci ( Leonard.nyholm@gmail.com )
+
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 try:
     from argparse import ArgumentParser
     from asyncio import get_event_loop
@@ -433,7 +464,7 @@ if __name__ == "__main__":
         help="Enable proxy, default is TOR proxy socks5://127.0.0.1:9050",
     )
     parser.add_argument("-t", "--target", type=str, help="Targeted Top Level Domain")
-    parser.add_argument("-d", "--dorks", type=int, help="amount of dorks")
+    parser.add_argument("-d", "--dorks", type=int, help="Amount of dorks")
     parser.add_argument("-P", "--pages", type=int, help="Number of pages to go through")
     args = parser.parse_args()
     if args.proxy:
